a58cf24978688d09f13cac67c14ea823c8cd5c64
MekhalaN <nandirajumekhala21@gmail.com>
2021-04-05 11:51:29 +0000

    If condition

--- Demo 1.js
+++ Demo 1.js
@@ -1,1 +1,16 @@
 
+console.log("hellos");  
+
+x=3
+y=10
+z=x+y
+
+console.log(z);
+
+let userdata = ["selenium",10,2005];
+console.log(userdata[0]);
+console.log(userdata[1]);
+console.log(userdata[3]);
+
+userdata.push(1,2,3);
+console.log(userdata);
